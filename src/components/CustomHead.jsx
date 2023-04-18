7828296c9593d91e80a25d9d6b75db764b3c3683
Eniolayo <eniolayodeji@gmail.com>
2023-04-18 19:40:17 +0100

    fix

--- src/components/CustomHead.jsx
+++ src/components/CustomHead.jsx
@@ -4,10 +4,8 @@ import React from "react";
 export default function CustomHead({ pageName, descriptionContent }) {
   return (
     <Head>
-      {/* <title>{`Certitude || ${pageName}`}</title> */}
-      <title>Certitude</title>
-      {/* <meta name="description" content={descriptionContent} /> */}
-      <meta name="description" content="desc" />
+      <title>{`Certitude || ${pageName}`}</title>
+      <meta name="description" content={descriptionContent} />
     </Head>
   );
 }
