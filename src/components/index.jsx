6c641eeb3b0299588ba70c7145eea286499c230e
Eniolayo <eniolayodeji@gmail.com>
2023-04-18 19:38:56 +0100

    fix

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,5 @@ import Header from "./Header";
 import Footer from "./Footer";
 import SubHeader from "./SubHeader";
 import MoreInfo from "./MoreInfo";
-
-export { Header, Footer, SubHeader, MoreInfo };
+import CustomHead from "./CustomHead";
+export { Header, Footer, SubHeader, MoreInfo, CustomHead };
